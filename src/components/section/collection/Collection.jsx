5a03ac79783d1339d73e11468050142804534402
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-27 18:45:00 -0400

    commit

--- src/components/section/collection/Collection.jsx
+++ src/components/section/collection/Collection.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import CollectionStyleWrapper from "./Collection.style";
 import { ethers } from "ethers";
 import { isMetaMaskInstalled } from "../../../config";
@@ -11,20 +11,17 @@ const Collection = () => {
   const contractAddress = "0x3ac22795304a27edb04cfe2475dcef0c5c8b5539";
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
-  const nftContract = new ethers.Contract(contractAddress, contract, signer);
+  const nftContract = useMemo(() => new ethers.Contract(contractAddress, contract, signer), [contractAddress, signer]);
   const [userAddress, setUserAddress] = useState("");
   const [remaining, setRemaining] = useState(0);
   const [nftIds, setNftIds] = useState([]);
   const [nftBalance, setNftBalance] = useState(0);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const calculateRemainingItems = async () => {
       let totalMintedItems = await totalMintCount();
-      console.log(totalMintedItems);
       setRemaining(totalMintedItems);
     };
-
     calculateRemainingItems();
   }, []);
 
@@ -33,56 +30,51 @@ const Collection = () => {
       if (!isMetaMaskInstalled()) {
         return;
       }
-
       const address = await signer.getAddress();
       setUserAddress(address);
     };
-
     fetchUserAddress();
   }, [signer]);
 
   useEffect(() => {
     const fetchNftBalance = async () => {
       let balance = await getNftBalance(signer, nftContract);
-      console.log(balance);
       setNftBalance(balance);
     };
-
     fetchNftBalance();
   }, [signer, nftContract]);
 
   useEffect(() => {
     const fetchNftsByUser = async () => {
       let nfts = await getNftsByUser(signer, nftContract);
-      console.log(nfts);
       setNftIds(nfts);
     };
-
     fetchNftsByUser();
   }, [signer, nftContract]);
 
-return (
-<CollectionStyleWrapper>
-<div style={{marginBottom: '20px'}}>
-  <h2 style={{fontWeight: 'bold', fontSize: '44px', color: '#FFFFFF'}}>
-    Total Ringo's: <Counter end={nftBalance} duration={nftBalance} />
-  </h2>
-</div>
+  return (
+    <CollectionStyleWrapper>
+      <div style={{ marginBottom: "20px" }}>
+        <h2 style={{ fontWeight: "bold", fontSize: "44px", color: "#FFFFFF" }}>
+          Total Ringo's: <Counter end={nftBalance} duration={nftBalance} />
+        </h2>
+      </div>
 
-<div></div>
+      <div></div>
 
-  <div className="nft-container">
-    {nftIds.length > 0 && nftIds.map((tokenId) => (
-      <img
-        key={tokenId}
-        src={`https://bafybeianqc6hbhbjejh2dvww6bb2srbu37kyf5uuzs7ztflsfpiwkz5vha.ipfs.w3s.link/${tokenId}.png`}
-        className="nft-image"
-      />
-    ))}
-  </div>
-</CollectionStyleWrapper>
-);
-
+      <div className="nft-container">
+        {nftIds.length > 0 &&
+          nftIds.map((tokenId) => (
+            <img
+              key={tokenId}
+              src={`https://bafybeianqc6hbhbjejh2dvww6bb2srbu37kyf5uuzs7ztflsfpiwkz5vha.ipfs.w3s.link/${tokenId}.png`}
+              className="nft-image"
+              alt={`NFT with id ${tokenId}`}
+            />
+          ))}
+      </div>
+    </CollectionStyleWrapper>
+  );
 };
 
 export default Collection;
--- src/pages/collections.jsx
+++ src/pages/collections.jsx
@@ -5,23 +5,31 @@ import Header from "../components/section/header/v1/Header";
 import PageHeader from "../common/pageHeader/v4";
 import Collection from "../components/section/collection";
 import Footer from "../components/section/footer/v4";
-import MetamaskModal from "../common/modal/metamaskModal/MetamaskModal"; 
+import MetamaskModal from "../common/modal/metamaskModal/MetamaskModal";
 import WalletModal from "../common/modal/walletModal/WalletModal";
+import { isMetaMaskInstalled } from "../config";
 
 const CollectionsPage = () => {
-  const { walletModalvisibility, metamaskModal} =
-    useModal();
+  const { walletModalvisibility, metamaskModal } = useModal();
 
   return (
     <>
       <Layout>
-        <GlobalStyles /> 
+        <GlobalStyles />
         {metamaskModal && <MetamaskModal />}
         {walletModalvisibility && <WalletModal />}
         <Header />
         <PageHeader />
-        <Collection />
-       <Footer />
+        {isMetaMaskInstalled() ? (
+          <Collection />
+        ) : (
+<div style={{ color: "#fff", fontSize: "2rem", display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+  MetaMask is not installed. Please install MetaMask to access this feature.
+</div>
+
+
+        )}
+        <Footer />
       </Layout>
     </>
   );
